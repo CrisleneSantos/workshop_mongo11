b47930c386003b2515dbd9e19dff3d2958599086
Crislene Santos <28mariavalentina@gmail.com>
2023-09-10 11:28:58 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/com/educandoweb/workshopmongo2/WorkshopMongo11Application.java
@@ -0,0 +1,13 @@
+package com.educandoweb.workshopmongo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongo11Application.class, args);
+	}
+
+}
