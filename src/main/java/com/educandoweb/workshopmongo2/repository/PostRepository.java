565c5bc61dd68051220a30003c6ff4e2c2af8c1d
Crislene Santos <28mariavalentina@gmail.com>
2023-09-10 21:11:23 -0300

    Consulta simples com @Query

--- src/main/java/com/educandoweb/workshopmongo2/repository/PostRepository.java
+++ src/main/java/com/educandoweb/workshopmongo2/repository/PostRepository.java
@@ -4,6 +4,7 @@ package com.educandoweb.workshopmongo2.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.educandoweb.workshopmongo2.domain.Post;
@@ -11,6 +12,11 @@ import com.educandoweb.workshopmongo2.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	
 }
 
